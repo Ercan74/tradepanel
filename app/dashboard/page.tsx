3e8ecdb1a67c405f321bcb81e4b352105f6759e1
Ercan <ercnmkk35@gmail.com>
2026-05-18 10:34:13 +0300

    fix analyticsgrid props

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -78,7 +78,7 @@ export default function DashboardPage() {
 
             <VisualIntelligenceLayer signals={signals} />
 
-            <AnalyticsGrid trades={trades} />
+            <AnalyticsGrid />
           </div>
         </section>
       </div>
